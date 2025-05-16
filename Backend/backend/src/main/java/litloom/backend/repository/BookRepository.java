a23c8876d9c614371078287180bed96ba184ffa5
Sanduni0601 <sanduni06nisansala@gmail.com>
2025-05-16 20:02:11 +0530

    Update repositories

--- Backend/backend/src/main/java/litloom/backend/repository/BookRepository.java
+++ Backend/backend/src/main/java/litloom/backend/repository/BookRepository.java
@@ -1,12 +1,10 @@
 package litloom.backend.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import litloom.backend.model.Book;
 
-import java.util.List;
+public interface BookRepository extends JpaRepository<Book, Long>{
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookRepository extends JpaRepository<Book, Long> {
 }
+
--- Backend/backend/src/main/java/litloom/backend/repository/CartItemRepository.java
+++ Backend/backend/src/main/java/litloom/backend/repository/CartItemRepository.java
@@ -1,15 +1,15 @@
 package litloom.backend.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import litloom.backend.model.CartItem;
 
-import java.util.List;
-import java.util.Optional;
-
 @Repository
-public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+public interface CartItemRepository extends JpaRepository<CartItem, Long>{
     Optional<CartItem> findByUserIdAndBookId(Long userId, Long bookId);
     List<CartItem> findByUserId(Long userId);
 }
